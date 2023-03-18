3e12b76c575d85aefad5cd953ada66f48fe1c359
Seal4ever <123288117+Seal4ever@users.noreply.github.com>
2023-03-18 23:06:25 +0300

    Add files via upload

---
+++ mushroom_log.c
@@ -0,0 +1,14 @@
+#include "mushroom_log.h"
+#include <stdlib.h>
+#include <stdarg.h>
+
+void mushroom_log(const char* format, ...)
+{
+    char *msg = NULL;
+    va_list ap;
+    va_start(ap, format);
+    vasprintf(&msg, format, ap);
+    printf("%s\n", msg);
+    va_end(ap);
+    free(msg);
+}
---
+++ mushroom_log.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stdio.h>
+
+void mushroom_log(const char* format, ...) __printflike(1, 2);
